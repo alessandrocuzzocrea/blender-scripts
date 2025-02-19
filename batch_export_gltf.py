f5cb374a111deb0c839efedfdf69dd40b10a7f02
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2025-02-19 23:21:23 +0900

    Refine GLTF export script to deselect all objects before selection and change export setting to use selection

--- batch_export_gltf.py
+++ batch_export_gltf.py
@@ -20,9 +20,10 @@ obj_active = view_layer.objects.active
 # Select all visible objects except those with "-noexport" suffix
 selection = [obj for obj in bpy.context.visible_objects if not obj.name.endswith("-noexport")]
 
+bpy.ops.object.select_all(action='DESELECT')
+
 for obj in selection:
     obj.select_set(True)
-    
     # Some exporters only use the active object
     view_layer.objects.active = obj
 
@@ -33,7 +34,7 @@ for obj in selection:
     bpy.ops.export_scene.gltf(
         filepath=fn + ".gltf",
         export_format='GLTF_SEPARATE',  # Use separate textures
-        use_visible=True,
+        use_selection=True,
         export_apply=True,  # Apply modifiers
         export_materials='EXPORT',  # Ensure materials are included
         export_texture_dir=os.path.join(godot_dir, "textures")  # Directory for textures
